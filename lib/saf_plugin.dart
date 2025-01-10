52881c1ac470a852ecd8b4edeb0b12eef6a12be2
bgli100 <>
2025-01-10 17:53:22 +0800

    fix document import/export

--- lib/saf_plugin.dart
+++ lib/saf_plugin.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/services.dart';
+import 'package:file_picker_ohos/file_picker_ohos.dart';
 
 class SAFPlugin {
   static const platform = const MethodChannel('com.perol.dev/saf');
 
   static Future<String?> createFile(String name, String type) async {
+    if (Platform.isOhos) {
+      return await FilePicker.platform.saveFile(
+        fileName: name,
+        type: FileType.any
+      );
+    }
+
     final result = await platform
         .invokeMethod("createFile", {'name': name, 'mimeType': type});
     if (result != null) {
@@ -16,20 +22,28 @@ class SAFPlugin {
   }
 
   static Future<void> writeUri(String uri, Uint8List data) async {
+    if (Platform.isOhos) {
+      return await FilePicker.platform.writeFile(
+        uri: uri,
+        bytes: data
+      );
+    }
     return platform.invokeMethod("writeUri", {'uri': uri, 'data': data});
   }
 
   static Future<Uint8List?> openFile() async {
-    if (Platform.isIOS) {
-      try {
-        FilePickerResult? result = await FilePicker.platform.pickFiles();
-        if (result != null) {
-          return File(result.files.single.path!).readAsBytes();
-        }
-      } catch (e) {}
-      return null;
+    if (Platform.isOhos) {
+      final FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ["json"],
+        withData: true
+      );
+      if (result != null)
+        return result.files.first.bytes;
+      else 
+        return null;
     }
-    return platform
-        .invokeMethod<Uint8List>("openFile", {'type': "application/json"});
+
+    return platform.invokeMethod<Uint8List>("openFile", {'type': "application/json"});
   }
 }
