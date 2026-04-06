88c2ae4afa07ff6b52ca188be14aa80987de1eaa
bgli100 <7760499+bgli100@users.noreply.github.com>
2026-04-06 13:30:55 +0800

    build fix

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
 $env:CC="$PWD\buildtool\aarch64-unknown-linux-ohos-clang.cmd"
 $env:AR="$PWD\buildtool\llvm-ar.cmd"
 $env:RUSTFLAGS="--cfg reqwest_unstable"
+
+cd .\plugins\rhttp\
+dart run build_runner build --delete-conflicting-outputs
+cd ..\..\
+
 dart run build_runner build --delete-conflicting-outputs
 flutter build hap --target-platform ohos-arm64 --release
